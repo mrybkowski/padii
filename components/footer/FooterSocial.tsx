208126f67927ec7805f07e5a6fe5ac09deef53e7
mrybkowski <marcin.artur.rybkowski@gmail.com>
2025-07-18 01:53:11 +0200

    Poprawa social media w stopce

--- components/footer/FooterSocial.tsx
+++ components/footer/FooterSocial.tsx
@@ -1,20 +1,25 @@
 import { Facebook, Instagram, Twitter } from "lucide-react";
-import { Button } from "../ui/button";
+import Link from "next/link";
+
+const socials = [
+  { icon: Facebook, href: "https://facebook.com" },
+  { icon: Instagram, href: "https://instagram.com" },
+  { icon: Twitter, href: "https://twitter.com" },
+];
 
 export function FooterSocials() {
-  const icons = [Facebook, Instagram, Twitter];
-
   return (
     <div className="flex space-x-4">
-      {icons.map((Icon, index) => (
-        <Button
+      {socials.map(({ icon: Icon, href }, index) => (
+        <Link
           key={index}
-          variant="ghost"
-          size="sm"
-          className="text-gray-400 hover:text-white"
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-10 h-10 flex items-center justify-center rounded-full bg-white/10 hover:bg-white/20 transition-all duration-200"
         >
-          <Icon className="h-5 w-5" />
-        </Button>
+          <Icon className="h-5 w-5 text-white" />
+        </Link>
       ))}
     </div>
   );
